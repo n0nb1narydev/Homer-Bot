e58aa4757b79aaf5f18ebd3755ea221741d3e406
Mel Avery Rumsey <melsrumsey@gmail.com>
2022-05-12 11:21:17 -0700

    testing gitbot

--- bot.py
+++ bot.py
@@ -5,17 +5,32 @@ import os
 import time
 import random
 import discord
-# from dotenv import load_dotenv
+from discord.ext import commands
+# import for slash commands
+from discord import app_commands
+
+
 from token_file import DISCORD_TOKEN
-from discord.ext import tasks, commands
 
 
 bot = discord.Client()
 
-# on_ready() will be called the message will be printed once client is ready for further action. (When bot switches from Offline to Online)
-@bot.event
-async def on_ready():
-    print("Bot Online.")
+class client(bot):
+    def __init__(self):
+        super().__init__()
+        self.synced = False #we use this so the bot doesn't sync commands more than once
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        print("Bot Online.")
+        if not self.synced: #check if slash commands have been synced 
+            await tree.sync(guild = discord.Object(id=974059269668343829)) #guild specific: leave blank if global (global registration can take 1-24 hours)
+            self.synced = True
+        print(f"We have logged in as {self.user}.")
+
+aclient = client()
+tree = app_commands.CommandTree(aclient)
+
 
 # execute Bot
 bot.run(DISCORD_TOKEN)
