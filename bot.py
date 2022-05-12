f8dcd2f4e5bf8e93d2c1419e01a892d48a850692
Mel Avery Rumsey <melsrumsey@gmail.com>
2022-05-12 11:48:09 -0700

    deleted test code

--- bot.py
+++ bot.py
@@ -12,24 +12,16 @@ from discord import app_commands
 
 from token_file import DISCORD_TOKEN
 
+intents = discord.Intents.default()
 
-bot = discord.Client()
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
 
-class client(bot):
-    def __init__(self):
-        super().__init__()
-        self.synced = False #we use this so the bot doesn't sync commands more than once
+@bot.event
+async def on_ready():
+    print("Bot Online")
 
-    async def on_ready(self):
-        await self.wait_until_ready()
-        print("Bot Online.")
-        if not self.synced: #check if slash commands have been synced 
-            await tree.sync(guild = discord.Object(id=974059269668343829)) #guild specific: leave blank if global (global registration can take 1-24 hours)
-            self.synced = True
-        print(f"We have logged in as {self.user}.")
 
-aclient = client()
-tree = app_commands.CommandTree(aclient)
 
 
 # execute Bot
